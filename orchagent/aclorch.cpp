d566e15a86b4064a2abdb0b2b3edf3ef56bdb666
arista-nwolfe <94405414+arista-nwolfe@users.noreply.github.com>
2024-02-05 13:17:12 -0500

    Allow L4 port range egress ACL rules on DNX (#3014)
    Allow L4 port range egress ACL rules on DNX (#3014)

--- orchagent/aclorch.cpp
+++ orchagent/aclorch.cpp
@@ -2202,9 +2202,10 @@ bool AclTable::addStageMandatoryRangeFields()
     SWSS_LOG_ENTER();
 
     string platform = getenv("platform") ? getenv("platform") : "";
+    string sub_platform = getenv("sub_platform") ? getenv("sub_platform") : "";
     auto match = SAI_ACL_TABLE_ATTR_FIELD_ACL_RANGE_TYPE;
 
-    if ((platform == BRCM_PLATFORM_SUBSTRING) &&
+    if ((platform == BRCM_PLATFORM_SUBSTRING) && (sub_platform != BRCM_DNX_PLATFORM_SUBSTRING) &&
         (stage == ACL_STAGE_EGRESS))
     {
         return false;
