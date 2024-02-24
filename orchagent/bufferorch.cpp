913217377260353c9b08f1d2a0dadc51c4399a2a
saksarav-nokia <sakthivadivu.saravanaraj@nokia.com>
2024-02-23 20:31:46 -0500

    Fixed orchagent crash in VM with the Qos BUFFER_QUEUE|system-port|Queue-id-range config (#3050)
    * Fixed orchagent crash in VM with the Qos BUFFER_QUEUE|system-port|Queue-id-range config

--- orchagent/bufferorch.cpp
+++ orchagent/bufferorch.cpp
@@ -966,8 +966,10 @@ task_process_status BufferOrch::processQueue(KeyOpFieldsValuesTuple &tuple)
                         return handle_status;
                     }
                 }
-                // create/remove a port queue counter for the queue buffer
-                else
+                // create/remove a port queue counter for the queue buffer.
+                // For VOQ chassis, flexcounterorch adds the Queue Counters for all egress and VOQ queues of all front panel and system ports
+                // to  the FLEX_COUNTER_DB irrespective of BUFFER_QUEUE configuration. So Port Queue counter needs to be updated only for non VOQ switch.
+                else if (gMySwitchType != "voq")
                 {
                     auto flexCounterOrch = gDirectory.get<FlexCounterOrch*>();
                     auto queues = tokens[1];
