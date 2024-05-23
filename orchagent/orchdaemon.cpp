de9baac1e0c1e52e8626bd43ac4e90e2373e4c81
Rajkumar-Marvell <54936542+rajkumar38@users.noreply.github.com>
2024-05-24 02:27:55 +0530

    [Marvell] PFC Detect lua script (#3160)
    *[Marvell] PFC Detect lua script (#3160)
    What I did
    Add PFC detect lua script for Marvell platform.
    Why I did it
    Support PFC Detect functionality

--- orchagent/orchdaemon.cpp
+++ orchagent/orchdaemon.cpp
@@ -581,6 +581,7 @@ bool OrchDaemon::init()
     }
     else if ((platform == INVM_PLATFORM_SUBSTRING)
              || (platform == BFN_PLATFORM_SUBSTRING)
+             || (platform == MRVL_PLATFORM_SUBSTRING)
              || (platform == NPS_PLATFORM_SUBSTRING))
     {
 
@@ -612,7 +613,7 @@ bool OrchDaemon::init()
 
         static const vector<sai_queue_attr_t> queueAttrIds;
 
-        if ((platform == INVM_PLATFORM_SUBSTRING) || (platform == NPS_PLATFORM_SUBSTRING))
+        if ((platform == INVM_PLATFORM_SUBSTRING) || (platform == NPS_PLATFORM_SUBSTRING) || (platform == MRVL_PLATFORM_SUBSTRING))
         {
             m_orchList.push_back(new PfcWdSwOrch<PfcWdZeroBufferHandler, PfcWdLossyHandler>(
                         m_configDb,
