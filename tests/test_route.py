835e576dbaca4391277002f1856db629f91d783f
Stepan Blyshchak <38952541+stepanblyschak@users.noreply.github.com>
2024-05-28 23:46:17 +0300

    [tests] mark suppress-fib-pending VS test as expected to fail
    [tests] mark suppress-fib-pending VS test as expected to fail

--- tests/test_route.py
+++ tests/test_route.py
@@ -1073,7 +1073,7 @@ class TestFpmSyncResponse(TestRouteBase):
         route_entry = json.loads(output)
         return bool(route_entry[route][0].get('offloaded'))
 
-    @pytest.mark.xfail(reason="Requires VS docker update in https://github.com/sonic-net/sonic-buildimage/pull/12853")
+    @pytest.mark.xfail(reason="BGP suppress FIB disabled on master/202405 - https://github.com/sonic-net/sonic-buildimage/issues/19092")
     @pytest.mark.parametrize("suppress_state", ["enabled", "disabled"])
     def test_offload(self, suppress_state, setup, dvs):
         route = "1.1.1.0/24"
